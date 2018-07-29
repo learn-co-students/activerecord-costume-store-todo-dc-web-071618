68626dad1fd0e0c52d3c733041b39543e4b548fb
github username <github email address>
2018-07-29 14:02:52 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,14 @@
-# Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+end
+
+
+#
+# The costume_stores table will have seven columns:
+#
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
